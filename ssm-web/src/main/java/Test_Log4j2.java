0bdb100207d5490b4bbfc4f6b1c66735ea02cfb7
czp <3056604571@qq.com>
2019-10-10 19:58:33 +0800

    Initial commit

---
+++ ssm-web/src/main/java/Test_Log4j2.java
@@ -0,0 +1,26 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Test_Log4j2 {
+    static Logger logger = LogManager.getLogger(LogManager.ROOT_LOGGER_NAME);
+
+    public static void main(String[] args) {
+        for(int i=0; i<200; i++){
+            logger.trace("trace message");
+            logger.debug("debug message");
+            logger.info("info message");
+            logger.warn("warn message");
+            logger.error("error message");
+            logger.fatal("fatal message");
+        }
+        System.out.println("Hello World!");
+
+        try {
+//            等10秒钟
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
